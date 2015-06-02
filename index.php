198d60cb6d522caad39002fa666888d7cfe6b1d2
JoeNg <JoeNg@192.168.1.158>
2015-06-02 12:12:20 +0800

    Test by Joe

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ and open the template in the editor.
         <title></title> 
     </head>
     <body>
-        NUSPlan - Plan to be efficient ;) TEST TEST TEST by boon jun
+        NUSPlan - Plan to be efficient ;)
         <a href="/aboutus.php">About us</a>
         Steps to do GIT
-        1. Always PULL first
+        1. Always PULL (Not Pull to upstream)
         2. Check if we working on same file - If No, continue. If yes, see who has higher priority
         3. Do ur work
         4. COMMIT
-        5. PUSH
+        5. PUSH (Not Push to upstream)
     </body>
 </html>
