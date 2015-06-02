aeadd0b1504f341ff38ba4e30796368d17bfbc8e
JoeNg <JoeNg@192.168.1.158>
2015-06-02 12:03:59 +0800

    Test by Joe

--- index.php
+++ index.php
@@ -12,5 +12,11 @@ and open the template in the editor.
     <body>
         NUSPlan - Plan to be efficient ;) TEST TEST
         <a href="/aboutus.php">About us</a>
+        Steps to do GIT
+        1. Always PULL first
+        2. Check if we working on same file - If No, continue. If yes, see who has higher priority
+        3. Do ur work
+        4. COMMIT
+        5. PUSH
     </body>
 </html>
