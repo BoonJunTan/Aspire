db52703f8a9c02688b60212f45c6695f175ec9d3
Boon Jun Tan <boonjuntan@u.nus.edu>
2015-06-03 20:13:57 +0800

    Test by boon jun

--- testing.php
+++ testing.php
@@ -2,37 +2,17 @@
 
 $path = "/assets/json/201415moduleList.json";
 
-//$finalpath = set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-
-echo get_include_path();
-
-echo "<br>";
-
-echo $finalpath;
-
-echo "<br>";
-
-$finalpath = $_SERVER['REQUEST_URI'] . "/assets/json/201415moduleList.json";
-
-echo $finalpath;
-
-echo "<br>";
+$finalpath = "../nusplan/assets/json/201415moduleList.json";
 
 $string = file_get_contents($finalpath);
 
-//echo file_get_contents("../assets/json/201415moduleInformation.json");
-
-//echo $string;
-
-echo ini_get('include_path');
-
 $json_a = json_decode($string, true);
 
-var_dump($json_a);
+//print_r($json_a);
+// To verify that there is something -> var_dump($json_a);
 
-foreach ($json_a as $key => $value){
-    echo  $key . ':' . $value;
-    
-}
-
+for ($i = 0; $i <= count($json_a); $i++) {
+    echo $json_a[$i]["ModuleCode"];
+    echo "<br>";
+} 
 ?>
