6804b78309f0db6bcffe4ed95131e7ed7ea6f4e8
Boon Jun Tan <boonjuntan@u.nus.edu>
2015-06-15 23:42:59 +0800

    Configuring Database

--- requirements.php
+++ requirements.php
@@ -27,8 +27,6 @@ and open the template in the editor.
 
         $conn = new mysqli($server, $username, $password, $db);
 
-        $conn = new mysqli($server, $username, $password, $db);
-
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
