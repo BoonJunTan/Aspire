07259e1695e72118988f73a324cdf35d55c28751
JoeNg <JoeNg@192.168.1.158>
2015-06-24 16:23:17 +0800

    15th June Commit

--- index.php
+++ index.php
@@ -1,16 +1,144 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
     "http://www.w3.org/TR/html4/strict.dtd">
 
-<?php
-?>
-
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>NUSPlan - Plan to be efficient ;)</title> 
-    </head>
-    <body>
-        
-    <br>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NUS Plan - Plan to be Efficient (:</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+   
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Plugin CSS -->
+    
+    <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
+    <!-- Custom CSS -->
+   
+    <link href="assets/css/creative.css" rel="stylesheet" type="text/css"/>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">We've got what you need!</h2>
+                    <hr class="light">
+                    <p class="text-faded">NUSPlan has what you want when it comes to module planning for the rest of your studying journey!
+                    Be it searching for a module, planning your module, or finding more about how NUSPlan works!
+                    </p>
+                    <a href="" class="btn btn-default btn-xl">Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3><a href ="aboutUs.php">About Us</a></h3>
+                        <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <h3><a href="planCurriculumView.php">Plan your Modules<a></h3>
+                        <p class="text-muted">You can use this theme as is, or you can make changes!</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3><a href="curriculumView.php">Course Requirements</a></h3>
+                        <p class="text-muted">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <!-- <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Star -->
+                        <h3><a href ="moduleInfoView.php"> Search for Module</a></h3>
+                        <p class="text-muted">You have to make your websites with love these days!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   
+    <aside class="bg-dark">
+        <div class="container text-center">
+            
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!
+                    <p>If not you can leave your feedback at the form below!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>+65-9755 5721</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:a0125464h@nus.edu.sg">feedback@nusplan.com</a></p>
+                    
+                    
+                    
+                </div>
+            <a href="#" class="btn btn-default btn-xl wow tada col-lg-4 col-md-4 col-md-offset-4">Download Now!</a>
+        </div>
+    </aside>
+
+
+
+    <!-- jQuery -->
+    
+    <script src="assets/js/jquery.js" type="text/javascript"></script>
+    <!-- Bootstrap Core JavaScript -->
+    
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- Plugin JavaScript -->
+    <script src="assets/js/jquery.easing.min.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.fittext.js" type="text/javascript"></script>
+    <script src="assets/js/wow.min.js" type="text/javascript"></script>
+    
+
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/js/creative.js" type="text/javascript"></script>
+    
 </body>
 </html>
