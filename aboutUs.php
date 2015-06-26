bb01acd18373c1b992d2f966b4a11c972643de0b
JoeNg <JoeNg@192.168.225.122>
2015-06-26 16:12:25 +0800

    15th June Commit

--- aboutUs.php
+++ aboutUs.php
@@ -36,14 +36,33 @@
                 <img src="assets/images/WanSin.jpg" alt="" width="300" height="300"/>
                     
                 </tr>
-                 <tr>
-                    
-                <p>This is me. And I love to create new things out of nowhere</p>
+                <tr>
+                <br>
+                <br>
+                <p>Hi, I am Joe Ng. Basically, I feel that NUSPlan has the capability to help every student in their module planning process and selection.
+                It helps every student to have a better control of their studying life and to prepare themselves for other future module planning in terms of exchange or even NOC.
+                This idea came about as both of us(the founders) often faced with similar problem, often changing our module preferences to suit each other. One change has to lead to many
+                changes and it gets messier and messier and we ended up feeling frustrated.
+                </p>
+                <p>
+                Join us on NUSPlan to explore the capabilites that it offers as we add on the many features or even enhancing the existing features! (:
+                </p>
                  
-                    </tr>    
+                </tr>    
                  
                 </td>
-              
+                <tr>
+                <br>
+                <br>
+                <img src="assets/images/BoonJun.jpg" alt="" width="300" height="300"/>
+                
+                </tr>
+                <tr>
+                <br>
+                <br>    
+                <p>This is me. And I love to create new things out of nowhere</p>
+                 
+                </tr>    
             </table>
           
         </div>
@@ -51,6 +70,8 @@
         <hr class="style18">
         <div>
         <h2>About NUSPlan</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/osQjStOAci0" frameborder="0" allowfullscreen></iframe>
+        
         </div>
     </body>
 </html>
